739b78056c86543b33ddb643fb72f298ebd9eece
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-07-17 19:44:04 +0300

    updated editProfile link and changed signin get to post method

--- server/config/routes.js
+++ server/config/routes.js
@@ -5,12 +5,12 @@ var helpers = require('./helpers.js');
 // exporting DB controller's functions
 module.exports = function(app, express){
 
-	app.get('/api/users/signin', userController.signin);
+	app.post('/api/users/signin', userController.signin);
 	app.get('/api/users/signedin', userController.checkAuth);
 	app.get('/api/users', userController.getAllUsers);
 	app.post('/api/users', userController.newUser);
 	app.post('/api/users/forget', userController.forgetPassUser);
-	app.post('/api/users/update', userController.editProfile);
+	app.post('/api/users/editProfile', userController.editProfile);
 
 	app.get('/api/blogs', blogController.getAllBlogs);
 	app.post('/api/blogs', blogController.newBlog);
