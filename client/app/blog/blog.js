16e52be8f76e02576b8a604301e28f26a0ef10d9
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-07-21 01:44:31 +0300

    Added basic html tags for the blog and implemented the initalize function in the controller

--- client/app/blog/blog.js
+++ client/app/blog/blog.js
@@ -1,5 +1,32 @@
 angular.module('RBKme.blog', [])
 
-.controller('BlogController', function ($scope) {
-  // Your code here
+.controller('BlogController', function ($scope,Blogs,Users) {
+	$scope.data = {};
+
+	$scope.initalize = function(){
+		Blogs.getAll()
+		.then(function(blogs){
+			$scope.data.blogs = blogs;
+			Users.getAll()
+			.then(function(users){
+				for(var i=0; i<$scope.data.blogs.length; i++){
+					for(var j=0; j<users.length; j++){
+						if($scope.data.blogs[i].from === users[j].username){
+							$scope.data.blogs[i].image = users[j].image;
+							$scope.data.blogs[i].name = users[j].firstName + ' ' + users[j].lastName;
+							break;
+						}
+					}
+				}
+			})
+			.catch(function(error){
+				console.log(error);
+			});
+		})
+		.catch(function(error){
+			console.log(errors);
+		});
+	};
+
+	$scope.initalize();
 });
