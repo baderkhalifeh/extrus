499aa90e553060881f51805b22fcfd1c733fbfde
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-07-21 00:01:27 +0300

    Implemented the profileView controller

--- client/app/profile/profileView.js
+++ client/app/profile/profileView.js
@@ -1,5 +1,22 @@
 angular.module('RBKme.profileView', [])
+.controller('profileViewController', function ($scope,$mdDialog,user) {
+  	
+  	$scope.user = {};
+  	$scope.user = user;
+	
+	$scope.hide = function() {
+    	$mdDialog.hide();
+	};
+	
+	$scope.cancel = function() {
+    	$mdDialog.cancel();
+	};
 
-.controller('profileViewController', function ($scope) {
-  // Your code here
+	// function to send back the answer to the main function
+	// which is showProfile in the app.js to know whether you
+	// want to edit the profile or not.
+	$scope.answer = function(answer) {
+    	$mdDialog.hide(answer);
+	};
 });
+
