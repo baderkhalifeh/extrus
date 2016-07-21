4a785e6521690c293d94c743fc7482320741aba0
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-07-21 17:47:42 +0300

    Implemented the forgotPassUser functionality

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,7 +1,9 @@
 angular.module('RBKme.auth', [])
 
-.controller('AuthController', function ($scope, $window, $location, $mdDialog, Auth) {
+.controller('AuthController', function ($scope, $window, $location, $mdDialog, $mdMedia, Auth) {
   
+  $window.username = '';
+
   $scope.user = {};
 
   // A flag to check if inputs filled or not
@@ -20,9 +22,10 @@ angular.module('RBKme.auth', [])
     	    Auth.signin($scope.user)
     	      .then(function (token) {
     	        $window.localStorage.setItem('com.RBKme', token);
+              $window.username = $scope.user.username;
               $scope.user.username = '';
               $scope.user.password = '';
-    	        $mdDialog.hide();
+    	        $mdDialog.hide('Success');
     	        $location.path('/');
     	      })
     	      .catch(function (error) {
@@ -36,21 +39,68 @@ angular.module('RBKme.auth', [])
   };
 
   $scope.signout = function () {
+    $window.username = '';
     Auth.signout();
   };
 
-  $scope.forgotPassword = function () {
-  	// $scope.user
-   //  $scope.userOrEmail = {
-   //    newPassword : $scope.newPassword 
-   //  };
-   //  Auth.forgotPassword ($scope.newPassword)
-   //  .then(function () {
-   //      $location.path('/');
-   //  })
-   //  .catch(function (error) {
-   //      console.error(error);
-   //  });
+  $scope.forgotPassword = function (ev) {
+      // defining the size of the pop-up to make it responsive
+      var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+
+      // showing the pop-up and giving the handling to the profileViewController
+      $mdDialog.show({
+        controller: 'AuthController',
+        templateUrl: 'app/auth/forgot.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose:true,
+        fullscreen: useFullScreen
+      })
+      .then(function(answer) {
+        // if the user chose to edit the profile he/she
+        // will be redirected to another pop-up for editing the info.
+        if(answer){
+          console.log(answer);
+          $location.path('/');
+          // $scope.editProfile(ev,user,$mdDialog);
+        }
+      }, function() {
+        $location.path('/');
+        $scope.status = 'You cancelled the dialog.';
+      });
+
+      // watching the changes on the window size to modify it instantly
+      // to be responsive
+      $scope.$watch(function() {
+        return $mdMedia('xs') || $mdMedia('sm');
+      }, function(wantsFullScreen) {
+        $scope.customFullscreen = (wantsFullScreen === true);
+      });
+   };
+
+   $scope.requestPass = function(){
+    var objToSend = {};
+    $scope.filled = true;
+    if(!$scope.user.forogtUser && !$scope.user.forgotEmail){
+      $scope.filled = false;
+      $scope.errorMsg = 'Please fill one of the boxes';
+    } else {
+      if($scope.user.forogtUser){
+        objToSend.username = $scope.user.forogtUser;
+      } else if($scope.user.forgotEmail){
+        objToSend.email = $scope.user.forgotEmail;
+      }
+      Auth.forgotPassword(objToSend)
+      .then(function(response){
+        console.log(response);
+      })
+      .catch(function(error){
+        console.log(error);
+      });
+      $scope.user.forogtUser = '';
+      $scope.user.forgotEmail = '';
+      $scope.hide();
+    }
    };
 
    $scope.hide = function() {
