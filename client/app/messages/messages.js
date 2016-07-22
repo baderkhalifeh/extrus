b914e27f47aa65efd69cae384d5412e9f7835608
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-07-22 18:58:57 +0300

    Implemented the initalize function in the messages controller

--- client/app/messages/messages.js
+++ client/app/messages/messages.js
@@ -1,5 +1,32 @@
 angular.module('RBKme.Msg', [])
 
-.controller('MsgController', function ($scope) {
+.controller('MsgController', function ($scope, Users, Messages){
 
+	$scope.data = {};
+
+	$scope.initalize = function(){
+		Users.getAll()
+		.then(function(users){
+			Messages.getMessagedFriends({username:window.username})
+			.then(function(list){
+				var MsgdFrineds = [];
+				for(var i=0; i<list.length; i++){
+					for(var j=0; j<users.length; j++){
+						if(users[j].username === list[i]){
+							MsgdFrineds.push(users[j]);
+						}
+					}
+				}
+				$scope.data.users = MsgdFrineds;
+			})
+			.catch(function(error){
+				console.log(error);
+			});
+		})
+		.catch(function(error){
+			console.log(error);
+		})
+	};
+
+	$scope.initalize();
 });
