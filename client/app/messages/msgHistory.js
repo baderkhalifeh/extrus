fa9b33590f17ddc55bdb5090f829170c845f1cff
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-07-23 00:25:12 +0300

    Working on getting users images to show in msgHistory

--- client/app/messages/msgHistory.js
+++ client/app/messages/msgHistory.js
@@ -1,13 +1,36 @@
 angular.module('RBKme.msgHistory', [])
 
-.controller('msgHistoryController', function ($scope, $mdDialog, Messages, fromToObj) {
+.controller('msgHistoryController', function ($scope, $mdDialog, Messages, Users, fromToObj) {
 
 	$scope.data = {};
+	$scope.user = {};
+	$scope.friend = {};
 	
 	$scope.initalize = function(){
 		Messages.getMessages(fromToObj)
 		.then(function(response){
 			$scope.data.msgs = response;
+			Users.getAll()
+			.then(function(users){
+				var counter = 0;
+				for(var i=0; i<users.length; i++){
+					if(users[i].username === fromToObj.username){
+						$scope.user = users[i];
+						counter++;
+					} else if(users[i].username === fromToObj.friend){
+						$scope.friend = users[i];
+						counter++;
+					}
+					if(counter >= 2){
+						break;
+					}
+				}
+				console.log($scope.user);
+				console.log($scope.friend);
+			})
+			.catch(function(error){
+				console.log(error);
+			});
 		})
 		.catch(function(error){
 			console.log(error);
